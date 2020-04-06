78f96134ebb0a9b6e5b3875576e727d8b4c44f83
Matthew Swanson <swansonmp@appstate.edu>
2020-04-06 17:30:37 -0400

    Thinking about ball physics

--- src/objects/ball.js
+++ src/objects/ball.js
@@ -1,10 +1,14 @@
 import * as THREE from './../lib/three.module.js';
 
+const ANGLE_INCREMENT = 1/32;
+
 export default class Ball {
   constructor(game) {
     this.game = game;
     
     this.position = new THREE.Vector3(-100, 10, -100);
+    this.angle = 0;
+    
   }
   
   update(deltaTime) {
@@ -13,6 +17,30 @@ export default class Ball {
     let y = this.game.graphics.getHeight(this.position);
     if (y != undefined) { this.position.y = y; }
     else { this.position.y = 1000; }
+  }
+  
+  reset(position) {
+    
+  }
+  
+  setLastPosition() {
+    this.lastPosition = this.position.copy();
+  }
+  
+  strike() {
+    
+  }
+  
+  incrementAngle() {
+    this.angle += ANGLE_INCREMENT;
+    if (this.angle >= Math.PI * 2)
+      this.angle -= Math.PI * 2;
+  }
+  
+  decrementAngle() {
+    this.angle -= ANGLE_INCREMENT;
+    if (this.angle <= 0)
+      this.angle += Math.PI * 2;
   }
   
 }
